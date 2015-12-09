a8158088a97a24494158e2c20724c0bd262eba1f
Jappie Klooster <jappieklooster@hotmail.com>
2015-12-09 13:44:10 +0100

    initial commit

---
+++ desktop/src/nl/jappieklooster/gdx/mapstare/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nl.jappieklooster.gdx.mapstare.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nl.jappieklooster.gdx.mapstare.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
