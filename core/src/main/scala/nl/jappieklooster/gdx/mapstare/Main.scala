8fe7ac8b0bc3e0dba569a999d276cfc01dd46de3
Jappie Klooster <jappieklooster@hotmail.com>
2015-12-21 22:29:46 +0100

    can now figure out the mouse position

--- core/src/main/scala/nl/jappieklooster/gdx/mapstare/Main.scala
+++ core/src/main/scala/nl/jappieklooster/gdx/mapstare/Main.scala
@@ -1,25 +1,35 @@
-package nl.jappieklooster.gdx.mapstare;
+package nl.jappieklooster.gdx.mapstare
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.{InputProcessor, Input, ApplicationAdapter, Gdx}
+import com.badlogic.gdx.graphics.{OrthographicCamera, Color, GL20, Texture}
+import com.badlogic.gdx.graphics.g2d.{BitmapFont, SpriteBatch}
+import nl.jappieklooster.gdx.mapstare.input.MouseTracker
+import com.badlogic.gdx.math.Vector3
 
 class Main extends ApplicationAdapter {
-	var batch:SpriteBatch = null
-	var img:Texture = null
+	lazy val batch:SpriteBatch = new SpriteBatch();
+	lazy val img:Texture = new Texture("badlogic.jpg")
 
+	lazy val font = new BitmapFont()
+	val mousetracker = new MouseTracker()
 	override def create() = {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font.setColor(Color.BLACK)
+		Gdx.input.setInputProcessor(mousetracker)
 	}
 
+	var x = 0.1f
+	lazy val cam = new OrthographicCamera(Gdx.graphics.getWidth, Gdx.graphics.getHeight)
 	override def render() = {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		val pos = cam.unproject(new Vector3(Gdx.input.getX + Gdx.graphics.getWidth/2, Gdx.input.getY - Gdx.graphics.getHeight/2, 0))
+		x += 0.1f
+		Gdx.gl.glClearColor(1, 1, 0, 1)
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, 0);
+		font.draw(batch, s"aaah", x, 2*x)
+		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+			font.draw(batch, s"(${pos.x},${pos.y})", pos.x,  pos.y  )
+		}
 		batch.end();
 	}
 }
