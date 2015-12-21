6adc76806ca6157aded95846bc3d85e0982e9b57
Jappie Klooster <jappieklooster@hotmail.com>
2015-12-21 21:12:19 +0100

    use scala for the library

--- core/src/main/scala/nl/jappieklooster/gdx/mapstare/Main.scala
+++ core/src/main/scala/nl/jappieklooster/gdx/mapstare/Main.scala
@@ -6,19 +6,17 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
+class Main extends ApplicationAdapter {
+	var batch:SpriteBatch = null
+	var img:Texture = null
+
+	override def create() = {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	override def render() = {
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
--- desktop/src/nl/jappieklooster/gdx/mapstare/desktop/DesktopLauncher.java
+++ desktop/src/nl/jappieklooster/gdx/mapstare/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package nl.jappieklooster.gdx.mapstare.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import nl.jappieklooster.gdx.mapstare.MyGdxGame;
+import nl.jappieklooster.gdx.mapstare.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new Main(), config);
 	}
 }
