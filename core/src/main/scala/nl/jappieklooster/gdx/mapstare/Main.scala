4fd6e4e0d17d23d276c8c68cd0d6eefcec388ee1
Jappie Klooster <jappieklooster@hotmail.com>
2015-12-23 18:20:29 +0100

    deleted a bunch of test code

--- core/src/main/scala/nl/jappieklooster/gdx/mapstare/Main.scala
+++ core/src/main/scala/nl/jappieklooster/gdx/mapstare/Main.scala
@@ -10,46 +10,30 @@ import nl.jappieklooster.gdx.mapstare.input.CamController
 
 class Main() extends ApplicationAdapter {
 	lazy val batch = new SpriteBatch()
-	lazy val img = new Texture("badlogic.jpg")
 
-	lazy val text = new Pixmap(40,40, Pixmap.Format.RGBA8888)
-	lazy val custom = new Texture(text)
 	lazy val font = new BitmapFont()
 	lazy val maprendeer = new OrthogonalTiledMapRenderer(new TmxMapLoader().load("map.tmx"))
 	lazy val cam = Cam.cam
 	override def create() = {
 		font.setColor(Color.BLACK)
-
-		text.setColor(Color.rgba8888(Color.RED))
-		text.drawLine(0,4,40,40)
-		text.drawLine(3,5,40,40)
-		text.drawLine(4,10,40,40)
 		Gdx.input.setInputProcessor(new CamController())
 	}
 
-	var x = 0.1f
 	var positions = List[(Texture,Float,Float)]()
 	override def render() = {
 		val pos = cam.mouseScreenPos()
-		x += 0.1f
 		Gdx.gl.glClearColor(1, 1, 0, 1)
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+		cam.cam.update()
 		maprendeer.setView(cam.cam)
 		maprendeer.render()
 		batch.begin()
-		batch.draw(img, x, 0)
-		font.draw(batch, s"aaah", x, 2*x)
 		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
 			font.draw(batch, s"(${pos.x},${pos.y})", pos.x,  pos.y  )
-			positions = (custom, pos.x,pos.y) :: positions
-		}
-		if(Gdx.input.isButtonPressed(Input.Buttons.RIGHT)){
-			positions = Nil
 		}
 		for(pos <- positions){
 			batch.draw(pos._1, pos._2, pos._3)
 		}
 		batch.end()
-		cam.cam.update()
 	}
 }
--- core/src/main/scala/nl/jappieklooster/gdx/mapstare/input/CamController.scala
+++ core/src/main/scala/nl/jappieklooster/gdx/mapstare/input/CamController.scala
@@ -1,16 +1,16 @@
 package nl.jappieklooster.gdx.mapstare.input
 
-import com.badlogic.gdx.Input
+import com.badlogic.gdx.Input._
 
 import nl.jappieklooster.gdx.mapstare.Cam
 import nl.jappieklooster.gdx.mapstare.model.Direction._
 
 class CamController(implicit cam:Cam) extends NotAnInputProcessor{
 	override def keyDown(keycode: Int): Boolean = keycode match {
-		case Input.Keys.LEFT => cam.move(Left); true
-		case Input.Keys.RIGHT => cam.move(Right); true
-		case Input.Keys.UP => cam.move(Up); true
-		case Input.Keys.DOWN => cam.move(Down); true
+		case Keys.LEFT  => cam.move(Left); true
+		case Keys.RIGHT => cam.move(Right); true
+		case Keys.UP | Keys.W => cam.move(Up); true
+		case Keys.DOWN => cam.move(Down); true
 		case _ => false
 	}
 }
