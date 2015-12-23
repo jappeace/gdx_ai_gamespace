b560d12b24815d4c273112c2a8e6932a975af806
Jappie Klooster <jappieklooster@hotmail.com>
2015-12-23 18:30:03 +0100

    this works somewhat bettar although no arrows now because

--- core/src/main/scala/nl/jappieklooster/gdx/mapstare/input/CamController.scala
+++ core/src/main/scala/nl/jappieklooster/gdx/mapstare/input/CamController.scala
@@ -6,11 +6,11 @@ import nl.jappieklooster.gdx.mapstare.Cam
 import nl.jappieklooster.gdx.mapstare.model.Direction._
 
 class CamController(implicit cam:Cam) extends NotAnInputProcessor{
-	override def keyDown(keycode: Int): Boolean = keycode match {
-		case Keys.LEFT | Keys.A  => cam.move(Left); true
-		case Keys.RIGHT | Keys.D => cam.move(Right); true
-		case Keys.UP | Keys.W => cam.move(Up); true
-		case Keys.DOWN | Keys.S => cam.move(Down); true
+	override def keyTyped(char:Char):Boolean = char match {
+		case 'a'  => cam.move(Left); false
+		case 'd' => cam.move(Right); false
+		case 'w' => cam.move(Up); false
+		case 's' => cam.move(Down); false
 		case _ => false
 	}
 }
