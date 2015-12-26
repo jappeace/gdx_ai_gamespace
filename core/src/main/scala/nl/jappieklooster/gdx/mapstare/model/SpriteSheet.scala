b91abd04eaedb6eb135a96449b3ee03392448942
Jappie Klooster <jappieklooster@hotmail.com>
2015-12-27 00:22:15 +0100

    no refacotirngs, we're doing it dirty

--- core/src/main/scala/nl/jappieklooster/gdx/mapstare/model/SpriteSheet.scala
+++
@@ -1,9 +0,0 @@
-package nl.jappieklooster.gdx.mapstare.model
-
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.Texture
-
-case class SpriteSheet(frameCount:Int, frameWidth:Int, frameHeight:Int, textureName:String) {
-	lazy val texture = new Texture(Gdx.files.internal(textureName))
-
-}
