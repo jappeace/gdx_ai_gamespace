65c47ac7da7e0681cb76beabb48c3e96a2a77bb3
Jappie Klooster <jappieklooster@hotmail.com>
2015-12-27 18:35:48 +0100

    comments

--- core/src/main/scala/nl/jappieklooster/gdx/mapstare/input/OnClick.scala
+++ core/src/main/scala/nl/jappieklooster/gdx/mapstare/input/OnClick.scala
@@ -4,12 +4,22 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 
 
+/**
+ * A less verbose click listener
+ * @param function
+ */
 class OnClick(function: (InputEvent, Float,Float) => Unit) extends ClickListener{
 	override def clicked(event:InputEvent, x:Float, y:Float):Unit = {
 		function(event,x,y)
 	}
 }
 object OnClick{
+	/**
+	 * the allows passing of a function that doesn't care about arguments
+	 * (most common use case probably)
+	 * @param function
+	 * @return
+	 */
 	def apply(function:()=>Unit):OnClick=apply((a, b,c)=>function())
 	def apply(function: (InputEvent, Float,Float) => Unit)=new OnClick(function)
 }
