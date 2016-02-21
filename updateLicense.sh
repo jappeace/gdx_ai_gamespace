8521a6de53d1be89c85f6d6effeb8041c39a6e87
Jappie Klooster <jappieklooster@hotmail.com>
2016-02-21 23:35:43 +0100

    this one works

--- updateLicense.sh
+++ updateLicense.sh
@@ -2,5 +2,5 @@
 
 for file in `find -iname "*.scala"`;
 do 
-	diff <(head -n 17 LICENSE_ProgramFiles) <(head -n 17 $file) || echo "wrongs! Fixing the license " && cat LICENSE_ProgramFiles | cat - $file > /tmp/out && mv /tmp/out $file;
+	diff <(head -n 17 LICENSE_ProgramFiles) <(head -n 17 $file) || `cat LICENSE_ProgramFiles | cat - $file > /tmp/out && mv /tmp/out $file`;
 done
